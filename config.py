8c2550722f9e48bf63667eef2de4f2c885a7a375
ahsan javed <ahsan@ahsans-MacBook-Air.local>
2023-04-03 09:27:41 +0100

    azure subscription actions

---
+++ config.py
@@ -0,0 +1,9 @@
+subscription_id = ""
+management_group_id = ""
+resource_group_name  = ""
+vnet_name = ""
+vnet_cidr = ""
+tenant_id =""
+group_name = ""
+# az coomand to get the id "az role definition list --query "[?roleName=='Contributor'].{Name:roleName, Id:id}""
+role_definition_id = role_definition_id = ''.format(subscription_id)
---
+++ subscription.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import config
+import sys
+from azure.mgmt.managementgroups import ManagementGroupsAPI
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.subscriptions import SubscriptionClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.network.models import AddressSpace , VirtualNetwork
+
+# class AzureCredentials:
+#     def __init__(self):
+#         self.credential = DefaultAzureCredential()
+
+credential = AzureCliCredential()
+
+def border_msg(msg):
+    row = len(msg)
+    h = ''.join(['+'] + ['-' *row] + ['+'])
+    result= h + '\n'"|"+msg+"|"'\n' + h
+    print(result)
+
+def list_subscriptions():
+    try:
+        subscription_client = SubscriptionClient(credential)
+        subscriptions = subscription_client.subscriptions.list()
+        for sub in subscriptions:
+            border_msg(f"Name: {sub.display_name} ID: {sub.subscription_id}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+def move_subscription():
+    try:
+        print("Enabling the SecurityInsights before moving the subscription")
+        enable_resource_provider()
+        mgmt_group_client = ManagementGroupsAPI(credential)
+        mgmt_group_client.management_group_subscriptions.create(group_id=config.management_group_id,
+                                                                subscription_id=config.subscription_id)
+        print('Subscription {} has been moved to management group {}.'.format(config.subscription_id, config.management_group_id))       
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def enable_resource_provider():
+    try:
+        resource_client = ResourceManagementClient(credential,config.subscription_id)
+        resource_provider = 'Microsoft.SecurityInsights'
+        resource_client.providers.register(resource_provider)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+def create_vnet():
+    try:
+        resource_group_name = config.resource_group_name 
+        vnet_name = config.vnet_name
+        vnet_cidr = config.vnet_cidr
+        network_client = NetworkManagementClient(credential=credential, subscription_id=config.subscription_id)
+        
+        vnet_params = VirtualNetwork(
+        location="eastus",
+        address_space=AddressSpace(
+            address_prefixes=[vnet_cidr],
+            ),
+        )
+        # if Resource provider 'Microsoft.Network' is not enabled the following operation will also enable it
+        vnet_result = network_client.virtual_networks.begin_create_or_update(
+        resource_group_name=resource_group_name,
+        virtual_network_name=vnet_name,
+        parameters=vnet_params,
+        )
+        print(f"Virtual network '{vnet_result.result().name}' created.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+
+def create_resource_group():
+    try:
+        resource_client = ResourceManagementClient(credential, config.subscription_id)
+        if resource_client.resource_groups.check_existence(config.resource_group_name):
+            print("Resource group '{0}' already exists. Script stopped.".format(config.resource_group_name))
+            exit()
+        rg_result = resource_client.resource_groups.create_or_update(
+        config.resource_group_name, {"location": "eastus"}
+        )
+        print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+
+    
+    
+def main():
+    
+    def main_menu():
+        print("Welcome to the Main Menu")
+        print("1. List Subscriptions")
+        print("2. Move Subscriptions ")
+        print("3. Create Resource Group")
+        print("4. Create Vnet")
+        print("5. Exit")
+        while True:
+            try:
+                choice = input("Enter your choice: ")
+                choice = int(choice)
+                if choice in range(1, 6):
+                    return str(choice)
+                else:
+                    print("Invalid choice. Please enter a number between 1 and 5.")
+            except ValueError:
+                print("Invalid input. Please enter a number between 1 and 5.")
+
+    def list_sub():
+        list_subscriptions()
+
+    def move_sub():
+        move_subscription()
+
+    def resource_grp():
+        create_resource_group()
+    
+    def vnet():
+        create_vnet()
+
+    options = {
+        "1": list_sub,
+        "2": move_sub,
+        "3": resource_grp,
+        "4": vnet,
+        "5": lambda: print("Exiting...")
+    }
+
+    while True:
+        choice = main_menu()
+        options.get(choice)()
+        if choice == "5":
+            break
+
+if __name__ == "__main__":
+    main()
+
