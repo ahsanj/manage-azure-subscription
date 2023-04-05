fa2e665060fde7948c4ac4733821391096ceef7d
Ahsan Javed <ahsan@ahsanjaved.com>
2023-04-05 13:48:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,21 @@ vnet_name = 'testvnet'
 vnet_cidr = "10.0.0.0/16"
 
 # az command to get the id "az role definition list --query "[?roleName=='Contributor'].{Name:roleName, Id:id}""
-tags={'environment': 'cloud',
-      'env':'alpha'}
+
+
+############Tags for subscription############
+tags =  {
+    'env': 'dev',
+    'test': 'test1'
+    }
 
 ############Service Principal############
 client_id = ''
 tenant_id = ''
-role_definition_id_sp = "" #contributor
+
+role_definition_id_sp = "" #use the following command to get the contributor id
+# az command to get the id "az role definition list --query "[?roleName=='Contributor'].{Name:roleName, Id:id}
+
 principal_id_sp = '' #use the following command to get the principal id
 #az ad sp list --filter "displayName eq 'sp_name'"
 scope_sp = '/subscriptions/' + subscription_id
